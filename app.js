bd649bfc3caac6b175b43af5521efa34aa526d67
Matt <me@mattnull.com>
2012-10-01 20:59:48 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,124 @@
+/**
+ * TechSavvy 2012
+ * @author Matt Null
+ */
+
+var express = require('express')
+, app = express()
+, http = require('http')
+, connect = require('connect')
+, server = http.createServer(app)
+, io = require('socket.io').listen(server)
+, mu = require('mu2')
+, rest = require('getjson')
+, fs = require('fs')
+, env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+, render = function(view, data, callback){
+	callback = callback || function(){};
+	data = data || {};
+	view = view || '';
+
+	mu.compile(view, function (err, parsed) {
+		if (err) {
+		  throw err;
+		}
+
+		var buffer = '';
+
+		mu.render(parsed, data)
+		  .on('data', function (c) { buffer += c.toString(); })
+		  .on('end', function () {
+		  		callback(buffer); 
+		  });
+	});
+}
+, uglify = require('uglify-js')
+, crypto = require('crypto');
+
+// App Configuration
+app.configure('production', function(){
+	mu.root = __dirname + '/views';
+
+	app.use(connect.cookieParser());
+	app.use(connect.session({ secret: '', cookie: { maxAge: 3600000}})); //1 hour sessions
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.static(__dirname + '/public'));
+	app.use(app.router);
+});
+
+app.configure('development', function(){
+	mu.root = __dirname + '/views';
+
+	app.use(connect.cookieParser());
+	app.use(connect.session({ secret: '', cookie: { maxAge: 3600000}})); //1 hour sessions
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.static(__dirname + '/public'));
+	app.use(app.router);
+});
+
+//compress javascript on prod
+if(env == 'production'){ 
+	fs.readdir('public/javascripts', function(err, files){
+		
+		if(err) return;
+
+		var parser = require("uglify-js").parser;
+		var pro = require("uglify-js").uglify;
+
+		for(var i = 0; i < files.length; i++){
+
+			//ignore files that start with a "."
+			if(files[i].indexOf('.') == 0) continue;
+			
+			var file = 'public/javascripts/'+files[i];
+
+			var originalCode = fs.readFileSync(file, encoding='utf8');
+			var ast = parser.parse(originalCode); // parse code and get the initial AST
+			ast = pro.ast_mangle(ast); // get a new AST with mangled names
+			ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
+			var finalCode = pro.gen_code(ast); // compressed code here
+
+			fs.writeFileSync(file, finalCode, encoding='utf8');
+		}
+	});
+}
+
+//Socket.io configuration
+io.configure('production', function(){
+	io.enable('browser client minification');  // send minified client
+	io.enable('browser client etag');          // apply etag caching logic based on version number
+	io.enable('browser client gzip');          // gzip the file
+	io.set('log level', 1);                    // reduce logging
+	io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+	    'websocket'
+	  //, 'flashsocket'
+	  , 'htmlfile'
+	  , 'xhr-polling'
+	  , 'jsonp-polling'
+	]);
+});
+
+//Mongo DB Server
+var mongoDBUrl = '';
+var mongoCollections = [;
+var db = require('mongojs').connect(dbUrl, collections);
+
+//start the server
+server.listen(3333);
+
+/** Routes **/
+
+//index
+app.get('/', function(req, res){	
+
+});
+
+//Attach socket events
+io.sockets.on('connection', function(socket){
+
+});
+
+
+console.log("Express server listening on port 3333");
